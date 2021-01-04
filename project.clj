e5e78ce71c2778a11b3bd26286489c83b6c74bc1
Renato Alencar <renatoalencar.73@gmail.com>
2021-01-04 16:42:59 -0300

    Fix production build

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.9.1"
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.773"]
-                 [org.clojure/core.async  "0.4.500"]
+                 [org.clojure/core.async "0.4.500"]
                  [reagent "0.10.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
 
                  ;; Not that much stable, revisit sometimes.
@@ -20,45 +20,49 @@
   :source-paths ["src"]
 
   :cljsbuild {:builds
-              [{:id "dev"
+              [{:id           "dev"
                 :source-paths ["src"]
 
                 ;; The presence of a :figwheel configuration here
                 ;; will cause figwheel to inject the figwheel client
                 ;; into your build
-                :figwheel {:on-jsload "onlypartners.core/on-js-reload"
-                           ;; :open-urls will pop open your application
-                           ;; in the default browser once Figwheel has
-                           ;; started and compiled your application.
-                           ;; Comment this out once it no longer serves you.
-                           :open-urls ["http://localhost:3449/index.html"]}
+                :figwheel     {:on-jsload "onlypartners.core/on-js-reload"
+                               ;; :open-urls will pop open your application
+                               ;; in the default browser once Figwheel has
+                               ;; started and compiled your application.
+                               ;; Comment this out once it no longer serves you.
+                               :open-urls ["http://localhost:3449/index.html"]}
 
-                :compiler {:main onlypartners.core
-                           :target :bundle
-                           :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/out/index.js"
-                           :output-dir "resources/public/js/compiled/out"
-                           :bundle-cmd {:none ["npx.cmd" "webpack" "--mode=development"]
-                                        :default ["npx.cmd" "webpack"]}
-                           :source-map-timestamp true
-                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
-                           ;; https://github.com/binaryage/cljs-devtools
-                           :preloads [devtools.preload]}}
+                :compiler     {:main                 onlypartners.core
+                               :target               :bundle
+                               :asset-path           "js/compiled/out"
+                               :output-to            "resources/public/js/compiled/out/index.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :bundle-cmd           {:none    ["npx.cmd" "webpack" "--mode=development"]
+                                                      :default ["npx.cmd" "webpack"]}
+                               :source-map-timestamp true
+                               ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
+                               ;; https://github.com/binaryage/cljs-devtools
+                               :preloads             [devtools.preload]}}
                ;; This next build is a compressed minified build for
                ;; production. You can build this with:
                ;; lein cljsbuild once min
-               {:id "min"
+               {:id           "min"
                 :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/onlypartners.js"
-                           :main onlypartners.core
-                           :optimizations :advanced
-                           :pretty-print false}}]}
+                :compiler     {:main          onlypartners.core
+                               :target        :bundle
+                               :asset-path    "js/compiled/out"
+                               :output-to     "resources/public/js/compiled/out/index.js"
+                               :output-dir    "resources/public/js/compiled/min"
+                               :bundle-cmd    {:none    ["npx.cmd" "webpack" "--mode=production"]
+                                               :default ["npx.cmd" "webpack"]}
+                               :optimizations :advanced}}]}
 
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
 
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
+             :css-dirs ["resources/public/css"]             ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
              ;; :nrepl-port 7888
@@ -93,10 +97,10 @@
              ;; :server-logfile false
              }
 
-  :profiles {:dev {:dependencies [[binaryage/devtools "1.0.0"]
-                                  [figwheel-sidecar "0.5.20"]]
+  :profiles {:dev {:dependencies  [[binaryage/devtools "1.0.0"]
+                                   [figwheel-sidecar "0.5.20"]]
                    ;; need to add dev source path here to get user.clj loaded
-                   :source-paths ["src" "dev"]
+                   :source-paths  ["src" "dev"]
                    ;; need to add the compiled assets to the :clean-targets
                    :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                                      :target-path]}})
